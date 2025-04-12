f36ecf79fb78f719ff1406442ef0fff2e59119e5
vishalpandey055 <146988099+vishalpandey055@users.noreply.github.com>
2025-04-12 21:01:33 +0530

    Time: 44 ms (81.64%), Space: 56.4 MB (53.57%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+   function createCounter(init) {
+  let current = init;
+
+  return {
+    increment: function () {
+      current += 1;
+      return current;
+    },
+    decrement: function () {
+      current -= 1;
+      return current;
+    },
+    reset: function () {
+      current = init;
+      return current;
+    }
+  };
+};
+
